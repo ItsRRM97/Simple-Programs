ef96b2986f2647deea12f7077418a04bd55031f3
Roshan Raj Mishra <mishra.roshanraj@gmail.com>
2018-01-12 21:46:50 +0530

    Added more HackerEarth Solutions

---
+++ HackerEarth/Basic Programming/CyclicString.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>  
+  using namespace std;  
+  int main()  
+ {  
+    long long tc;  
+    cin>>tc;  
+    while(tc--){  
+      string s;  
+        cin>>s;  
+      long long n = s.length();  
+      cout<<(n*(n+1))/2<<"\n";  
+    }  
+    return 0;  
+  }
